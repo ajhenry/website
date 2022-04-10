30e18c9f387af858c3251959bf8fc3b040380980
Andrew Henry <andrew.henry@pitt.edu>
2022-04-09 22:41:46 -0400

    fix: apostro

--- components/Sections/About.section.tsx
+++ components/Sections/About.section.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image";
 
-
 const About = () => {
   return (
-    <div className="flex flex-row items-center justify-between px-3 my-8 font-sen">
+    <div className="font-sen my-8 flex flex-row items-center justify-between px-3">
       <div>
         <h1 className="text-3xl font-bold text-white">Andrew</h1>
         <h3 className="mt-1 text-lg font-medium text-gray-300">
-          Hey I'm ajhenry, a Full Stack Software Engineer and Open Source enthusiast 
+          Hey I&apos;m ajhenry, a Full Stack Software Engineer and Open Source
+          enthusiast
         </h3>
 
         <p className="mt-4 text-gray-400">
           Building modern web apps — proficient in TypeScript and Python
         </p>
-
       </div>
 
-      <div className="hidden custom:block">
+      <div className="custom:block hidden">
         <Image
           src="/assests/avatar.png"
           width="112"
